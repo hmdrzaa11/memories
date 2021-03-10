492bb2946bf49b646d5ca34888150340a5d75cf4
Hamid Reza <hmdrzaa11@gmail.com>
2021-03-11 01:20:43 +0330

    Dynamic Average Rating in place

--- models/Review.js
+++ models/Review.js
@@ -1,5 +1,6 @@
 let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
+let Memory = require("./Memory");
 
 let reviewSchema = new Schema({
   review: {
@@ -29,6 +30,43 @@ let reviewSchema = new Schema({
 
 //make sure a user can review only one time
 reviewSchema.index({ memory: 1, reviewer: 1 }, { unique: true });
+
+reviewSchema.statics.calculateRatingsAverage = async function (memId) {
+  //"this" refers to the Model
+  let stats = await this.aggregate([
+    {
+      $match: {
+        memory: memId,
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        ratingsAverage: { $avg: "$rating" },
+      },
+    },
+  ]);
+  let ratings = stats[0] ? stats[0].ratingsAverage : 4.5;
+  await Memory.findOneAndUpdate({ _id: memId }, { ratingsAvg: ratings });
+};
+
+reviewSchema.post("save", async function () {
+  let doc = this;
+  await doc.constructor.calculateRatingsAverage(doc.memory);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  let doc = await this.findOne();
+  this.review = doc;
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  if (this.review) {
+    //only if we successfully deleted a review
+    await this.review.constructor.calculateRatingsAverage(this.review.memory);
+  }
+});
 
 let Review = mongoose.model("Review", reviewSchema);
 
